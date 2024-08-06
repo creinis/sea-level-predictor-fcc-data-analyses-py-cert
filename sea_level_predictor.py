1169fd30ea9a33593be66d6aaf9a02ab59ce1cda
creinis <115181527+creinis@users.noreply.github.com>
2024-08-06 09:23:16 +0200

    feat: create a scatter plot using the Year column as the x-axis and the CSIRO Adjusted Sea Level column as the y-axis.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -7,6 +7,8 @@ def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
   
+    # Create scatter plot
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
