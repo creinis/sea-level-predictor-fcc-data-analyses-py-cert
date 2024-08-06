c2c710e3271a8e389f9b6feee4f7031b251682f3
creinis <115181527+creinis@users.noreply.github.com>
2024-08-06 09:22:26 +0200

    feat: define function to draw plot; save plot as a img file and return data for testing

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -2,6 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-# Read data from file
-df = pd.read_csv('epa-sea-level.csv')
+def draw_plot():
+
+    # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
   
+
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
